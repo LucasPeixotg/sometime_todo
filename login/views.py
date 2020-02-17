227dab75392d9757851aee8e24865695051df5de
Lucas Peixoto Gonçalves <Lucaspeixotopro@gmail.com>
2020-02-17 10:13:48 -0300

    Login after registration

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate, login
 
 def index_view(request, *args, **kwargs):
     return render(request, 'login/index.html', {
@@ -11,6 +12,13 @@ def register_view(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+
+            new_user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+            )
+            login(request, new_user)
+
             return redirect('/')
     else:
         form = RegisterForm()
